c8e139d401ba4893f3afc38abc03b85b5c05b60d
Nicolas Leconte <nihao@MacBook-Pro-6.local>
2023-10-07 00:48:34 +0900

    login, register and schedule work without errors

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from flask import render_template, redirect, url_for, flash, request
 from flask import Flask, flash, redirect, url_for, request, render_template
 from extensions import db, migrate
 import models  # Assuming models.py is in the same directory as app.py
-from models import User
+from models import User, Schedule
 from werkzeug.security import check_password_hash
 from werkzeug.security import generate_password_hash
 from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user, current_user
@@ -48,21 +48,6 @@ def register():
         return redirect(url_for('index'))
     return render_template('register.html')
 
-@app.route('/login', methods=['GET', 'POST'])
-def login():
-    if request.method == 'POST':
-        email = request.form.get('email')
-        password = request.form.get('password')
-        
-        user = User.query.filter_by(email=email).first()
-        if user and user.check_password(password):
-            session['user_id'] = user.id
-            flash('Logged in successfully!')
-            return redirect(url_for('index'))
-        else:
-            flash('Invalid email or password')
-            return redirect(url_for('login'))
-    return render_template('login.html')
 
 @app.route('/profile', methods=['GET', 'POST'])
 @login_required
@@ -116,20 +101,6 @@ def schedule():
 def load_user(user_id):
     return User.query.get(int(user_id))
 
-@app.route('/login', methods=['GET', 'POST'])
-def login():
-    if request.method == 'POST':
-        email = request.form.get('email')
-        password = request.form.get('password')
-        user = User.query.filter_by(email=email).first()
-
-        if user and user.check_password(password):
-            login_user(user)
-            return redirect(url_for('profile'))
-        
-        flash('Login Unsuccessful. Please check email and password', 'danger')
-
-    return render_template('login.html')
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
